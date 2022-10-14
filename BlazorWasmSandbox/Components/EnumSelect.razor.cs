1aa1364c8332dfb2064b4b51a6cf115f6c046bdf
Michael Bond <michael@bondcodes.com>
2022-10-13 21:18:08 -0400

    Fix up custom component binding

---
+++ BlazorWasmSandbox/Components/EnumSelect.razor.cs
@@ -0,0 +1,15 @@
+namespace BlazorWasmSandbox.Components;
+
+public partial class EnumSelect<TEnum> where TEnum : struct
+{
+    [Parameter]
+    public TEnum SelectedValue { get; set; } = default!;
+
+    [Parameter]
+    public EventCallback<TEnum> SelectedValueChanged { get; set; }
+
+    private Task OnSelectedValueChanged(ChangeEventArgs e) =>
+        Enum.TryParse(e?.Value?.ToString() ?? string.Empty, true, out TEnum enumValue)
+            ? SelectedValueChanged.InvokeAsync(enumValue)
+            : SelectedValueChanged.InvokeAsync();
+}
--- BlazorWasmSandbox/GlobalUsings.cs
+++ BlazorWasmSandbox/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using BlazorWasmSandbox;
 global using BlazorWasmSandbox.Models;
+global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
